a15bbc7ffd86186914196e966e7608b0dcf7ee9b
Oleg Khovayko <olegh@emercoin.com>
2025-08-09 22:30:56 -0400

    ADD: random_redirect

--- src/banmoron.c
+++ src/banmoron.c
@@ -2,6 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
 /*------------------------------------------------------------------------------*/
 // 404 CGI program for perform strike-back action.
@@ -24,7 +27,7 @@ struct rule {
   unsigned char	op_num;		// Action number (weapon)
 };
 
-#define USE_TEST	1
+#define USE_TEST	0
 
 // Do not ban computerd from LAN, we debug with them
 #define LAN_PREFIX "192.168."
@@ -33,26 +36,9 @@ struct rule {
 #define HMASK ((1 << 9) - 1)
 
 #define BANRULE(str, op) {str, sizeof(str) - 1, op},
-// Substring length: min=3, max=13
-struct rule rules[] = {
-  //-------xxxXXXXXXXXXX---
-//  BANRULE("setup.php",	  1)	// Ban PHP My Admin scanner
-//  BANRULE("xmlrpc.php",	  1)	// Ban WordPress XMLRPC bruteforecer
-//  BANRULE("command.php",  1)	// Ban - I don't know what, but found in httpd-access
-//  BANRULE("Detect.js.php",1)	// Ban - some new exploit, don't know details
-  BANRULE(".php",	  1)	// Ban - attempt to palp sever by multiple vulnerabilities
-  BANRULE("http://",	  1)	// Ban - proxy scanner
-  BANRULE("wallet",	  2)	// Send zip-bomb to wallet lovers
-  BANRULE("w00t",	  3)	// Ban Romanian PHP My Admin exploit
-  BANRULE("Unblock.cgi"	 ,3)	// Zip+Ban - Attempt hack into router
-  //-------xxxXXXXXXXXXX---
-};
-
-#define RULES_QTY (sizeof(rules) / sizeof(struct rule))
-
 /*------------------------------------------------------------------------------*/
 
-char *g_uri, *g_ip;
+const char *g_uri, *g_ip;
 
 const char htmlHead[] = 
 	"Content-Type: text/html; charset=ISO-8859-1\n\n"
@@ -63,44 +49,33 @@ const char htmlHead[] =
 /*------------------------------------------------------------------------------*/
 // Just Err404 print - no any counter-action
 void print_404(void) {
+ const char *print_ip = (g_ip == NULL)? "tty" : g_ip;
   printf(
 	"%s<h1>Page Not Found</h1>\n"
 	"<p>The requested URL [%s] was not found on this server.</p>\n"
-	"<p>Your IP: %s</p>\n"
-	"</body></html>\n", htmlHead, g_uri, g_ip);
+	"<p>Your IP is: [%s]</p>\n"
+	"</body></html>\n", htmlHead, g_uri, print_ip);
 } // print_404
-
-/*------------------------------------------------------------------------------*/
-// Common ban function. Must be called from filter-specific weapon
-// Release will be from crontab
-void ban_moron(char **parmList) {
-  if(strncmp(g_ip, LAN_PREFIX, sizeof(LAN_PREFIX) - 1) == 0) { // Preserve block LAN
-    printf("%sLAN IP is not blocked: %s\n</body></html>\n", htmlHead, g_ip);
-  } else {
-    printf("%sBlocked IP: %s\n</body></html>\n", htmlHead, g_ip);
-    fclose(stdout);
-
-    // Allow only [:.0-9A-Fa-f] in the IP to preserve possible REMOTE_ADDR hack
-    for(char c, *p = g_ip; (c = *p) != 0; p++) {
-      if(!(
-	    (c >= '0' && c <= '9') ||
-	     c == '.' || c == ':'  ||
-	    (c >= 'a' && c <= 'f') ||
-	    (c >= 'A' && c <= 'F')
-	 ))
-        exit(~0); // Hacked REMOTE_ADDR, unable to block. Report an error
-    } // for
-    execv(parmList[0], parmList);
-  } // else -- LAN
-} // ban_moron
 
 /*------------------------------------------------------------------------------*/
 // FreeBSD PF specified banner. Put moron's IP to pf-table "morons"
 // Release will be from crontab
 void ban_moron_pf(void) {
-  char *parmList[] = {"/sbin/pfctl", "-qt", "morons", "-T", "add", NULL, NULL};
-  parmList[5] = g_ip;
-  ban_moron(parmList);
+    if(g_ip == NULL) {
+        printf("TTY call is not blocked\n");
+        return;
+    }
+    if(strncmp(g_ip, LAN_PREFIX, sizeof(LAN_PREFIX) - 1) == 0) { // Preserve block LAN
+        printf("%sLAN IP is not blocked: %s\n</body></html>\n", htmlHead, g_ip);
+        return;
+    }
+    printf("%sBlocked IP: %s\n</body></html>\n", htmlHead, g_ip);
+    fclose(stdout);
+
+    char *parmList[] = {"/sbin/pfctl", "-qt", "morons", "-T", "add", NULL, NULL};
+    parmList[5] = (char *)g_ip;
+    execv(parmList[0], parmList);
+
 } // ban_moron_pf
 
 /*------------------------------------------------------------------------------*/
@@ -110,59 +85,114 @@ void ban_moron_pf(void) {
 // for overflow HTML parser on hacker's side
 void zip_bomb(void) {
   const char bomb_header[] = {
-    0x1f,0x8b,0x08,0x08,0x30,0x8d,0x72,0x59,0x02,0x03,0x62,0x6f,0x6d,0x62,0x2e,0x74,
-    0x78,0x74,0x00,0xed,0xca,0xc1,0x09,0xc3,0x30,0x10,0x04,0xc0,0xbf,0xaa,0x48,0x07,
-    0x6e,0xe0,0x50,0x2f,0x12,0x32,0xd8,0x60,0xe3,0x10,0xf4,0x89,0xab,0xb7,0x8c,0xd3,
-    0x42,0x7e,0xf3,0x38,0x96,0xd9,0xbd,0x58,0xfa,0xbe,0xe5,0x14,0xcb,0x5c,0xda,0x88,
-    0xbe,0xf6,0x6d,0xce,0xe7,0xfa,0x7e,0xd5,0x63,0xaf,0x31,0x3d,0x4e,0x31,0xfd,0xf6,
-    0x7a,0xb4,0xef,0xfd,0x56,0xea,0xa8,0xa3,0x7f,0xc6,0x35,0x66,0x66,0x66,0x66,0x66
+    0x1f,  0x8b,  0x08,  0x08,  0xfe,  0xd4,  0x97,  0x68,  0x02,  0x03,  0x62,  0x6d,  0x62,  0x2e,  0x74,  0x78,
+    0x74,  0x00,  0xed,  0xc7,  0xb1,  0x09,  0x80,  0x30,  0x10,  0x40,  0xd1,  0xde,  0x65,  0x5c,  0xe0,  0xb8,
+    0x5d,  0x12,  0x12,  0x48,  0x11,  0x11,  0x24,  0x8d,  0xdb,  0x2b,  0xb8,  0x83,  0xd5,  0x2b,  0x3e,  0x9f,
+    0x17,  0x63,  0x1d,  0x33,  0x63,  0xf4,  0xd2,  0x32,  0xf6,  0x6f,  0xf5,  0x6c,  0x77,  0x6e,  0xb1,  0x4a,
+    0x9d,  0x3d,  0x63,  0x5d,  0x6f,  0x8d,  0x99,  0x99,  0x99,  0x99,  0x99,  0x99,  0x99,  0x99,  0x99,  0x99,
   };
 
-  char bomb_body[1024];
-  memset(bomb_body, 0x66, 1024);
+  char bomb_body[256];
+  memset(bomb_body, 0x99, sizeof(bomb_body));
 
   puts(
+      "Status: 200 OK\n"
       "Content-Type: text/html; charset=ISO-8859-1\n"
       "Content-Encoding: gzip\n"
       );
 
-  fwrite(bomb_header, 16, 6, stdout); 
+  fwrite(bomb_header, sizeof(bomb_header), 1, stdout); 
 
-  // Send infinity zip, until iclient close connection
-  while(fwrite(bomb_body, 1024, 1, stdout) != 1);
+  // Send infinity zip, until client close connection
+  while(fwrite(bomb_body, sizeof(bomb_body), 1, stdout) == 1);
 
+  ban_moron_pf();
 } // zip_bomb
 
 /*------------------------------------------------------------------------------*/
-// Send zip-bomb and ban thereafter
-void zip_ban(void) {
-  zip_bomb();
+void random_redirect(void) {
+    const static char protos[4][6] = {
+        "https", "https", "http", "ftp"
+    };
+    uint32_t rnd_ip   = rand();
+    uint32_t rns_port = rand();
+    if((rnd_ip ^ rns_port) <= 0x00ffffff)
+        // 1 ticket of 256 wins bonus - 100G file!
+        printf("Status: 301 Moved Permanently\n"
+           "Location: http://speedtest.tele2.net/100GB.zip\n\n"); 
+    else
+        printf("Status: 301 Moved Permanently\n"
+           "Location: %s://%u.%u.%u.%u:%u/%x\n\n", 
+           protos[rns_port & 3],
+           (uint8_t)rnd_ip,
+           (uint8_t)(rnd_ip >> 8),
+           (uint8_t)(rnd_ip >> 16),
+           (uint8_t)(rnd_ip >> 24),
+           (rns_port >> 16),
+           (rns_port >> 2) & 0x3fff
+           );
   ban_moron_pf();
-} // zip_ban
+}
 
 /*------------------------------------------------------------------------------*/
 // Arsenal of weapons
-//				0		1	2	3
-const action_t arsenal[] = { print_404, ban_moron_pf, zip_bomb, zip_ban };
+//                                   0             1         2                3
+const action_t arsenal[] = { print_404, ban_moron_pf, zip_bomb, random_redirect };
+
+// Substring length: min=3, max=13
+struct rule rules[] = {
+  //-------xxxXXXXXXXXXX---
+  BANRULE("http://",      1)	// Ban - proxy scanner
+  BANRULE("wallet",	      2)	// Send zip-bomb to wallet lovers
+  BANRULE(".bak",         2)	// Zip+Ban - Backup lover - take backup!
+  BANRULE("etc/passwd",   2)	// Zip+Ban - passwd files lover
+  BANRULE("Unblock.cgi",  2)    // Zip+Ban - Attempt hack into router
+  BANRULE("w00t",	      3)	// Ban Romanian PHP My Admin exploit
+  BANRULE("../..",        3)	// Redirect
+  BANRULE(".php",	      3)	// 
+  BANRULE("wp-content",   3)	// 
+  BANRULE("test-cgi",     3)	// 
+  //-------xxxXXXXXXXXXX---
+};
+
+#define RULES_QTY (sizeof(rules) / sizeof(struct rule))
 
 /*------------------------------------------------------------------------------*/
-
 int main(int argc, char **argv) {
-  g_uri = getenv("REQUEST_URI");
-  g_ip  = getenv("REMOTE_ADDR");
+    g_uri = getenv("REQUEST_URI");
+    if((g_uri == NULL || *g_uri == 0) && argc > 1) {
+        // started from command line - check rules action, no real action calls
+        g_uri = argv[1];
+    }
+    if(g_uri == NULL || *g_uri == 0) {
+        print_404();
+        return 1; // Nothing to do
+    }
 
-  // get some random for universal hashing
-  int rnd = getpid();
+    srand(time(NULL) ^ getpid() + clock());
+    // get some random for universal hashing
+    int rnd = rand();
 
-#if USE_TEST
-  if(g_uri == NULL) {
-    // started from command line - check rules action, no real action calls
-    g_uri = argv[1];
-    g_ip  = (char*)0x1;
-  }
-#endif
+    g_ip  = getenv("REMOTE_ADDR");
+    if(g_ip == NULL || *g_ip == 0)
+        g_ip = NULL; // test IP, terminal call
+    else {
+        // Cleanup ip_addr with double conversion
+         char *new_ip = (char *)alloca(INET6_ADDRSTRLEN + (rnd >> 24));
+         const char *ret_ip = NULL;
+         struct sockaddr_storage ss;
+         if (inet_pton(AF_INET, g_ip, &ss) == 1)
+             ret_ip = inet_ntop(AF_INET, &ss, new_ip, INET6_ADDRSTRLEN);
+         else 
+         if(inet_pton(AF_INET6, g_ip, &ss) == 1)
+             ret_ip = inet_ntop(AF_INET6, &ss, new_ip, INET6_ADDRSTRLEN);
+         else
+             return 13; // Hack attempt 1 !!!
+         if(ret_ip == NULL)
+             return 14; // Hack attempt 2 !!!
+         g_ip = new_ip;
+    }
 
-  if(g_ip && g_uri) {
     static char htable[HMASK + 1]; // Hashtable for Rabin-Karp search algorithm
     int act_no = 0; // Default: print err404
 
@@ -176,32 +206,27 @@ int main(int argc, char **argv) {
     int hash = 0;
     signed char h2;
     for(const char *p = g_uri; *p; p++)
-      if((h2 = htable[hash = ((hash << 3) + (*p ^ rnd)) & HMASK]) && p - g_uri >= 2) {
-	int start = 0;
-	do {
-	  if(h2 < 0)
-            for(int r = start; r < RULES_QTY; r += 8) 
-              if(strncmp(p - 2, rules[r].str, rules[r].len) == 0) {
-	        act_no = rules[r].op_num;
-	        goto action;
-	      }
-	  start++;
-	} while(h2 <<= 1);
-      } // for+if
+        if((h2 = htable[hash = ((hash << 3) + (*p ^ rnd)) & HMASK]) && p - g_uri >= 2) {
+            int start = 0;
+            do {
+                if(h2 < 0)
+                    for(int r = start; r < RULES_QTY; r += 8) 
+                        if(strncmp(p - 2, rules[r].str, rules[r].len) == 0) {
+                            act_no = rules[r].op_num;
+                            goto action;
+                        }
+                start++;
+            } while(h2 <<= 1);
+        } // for+if
 
-    action:
-
+action:
 #if USE_TEST
-    if(g_ip == (char*)0x1) {
-      printf("Debug action=%d\n", act_no);
-      return act_no;
-    }
-    else
+    printf("Debug action=%d\n", act_no);
+    return act_no;
+#else
+    arsenal[act_no](); // Use weapon from the arsenal
 #endif
-      arsenal[act_no](); // Use weapon from the arsenal
-  }
-
-  return 0;
+    return 0;
 } // main
 
 /*------------------------------------------------------------------------------*/
--- src/bmtest.sh
+++ src/bmtest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-for i in /,0 None,0 setup.php,1 aa/bb/xsetup.php12345,1 wallet,2 walle,0 allet,0 \
-	xpwalletabc,2 1/2Detect.js.php2,1 2Detect.js.ph1,0 /hndUnblock.cgi,3 \
-	http://proxyscan,1 /w00tw00t,3
+for i in /,0 None,0 setup.php,3 aa/bb/xsetup.php12345,3 wallet,2 walle,0 allet,0 \
+	xpwalletabc,2 1/2Detect.js.php2,3 2Detect.js.ph1,0 /hndUnblock.cgi,2 \
+	http://proxyscan,1 /w00tw00t,3 z/../../a.cgi,3 test-cgi,3
 do 
   URI=${i%,*};
   RC=${i#*,};
